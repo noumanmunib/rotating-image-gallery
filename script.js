e19d8b58e1c73bb71b58733a906a5c0c04ab2548
nouman-munib <thesmartlad27@gmail.com>
2023-04-27 17:29:42 +0500

    second commit

--- script.js
+++ script.js
@@ -1,31 +1,72 @@
 const imageContainer = document.querySelector(".image-container");
 const btnNext = document.querySelector("#next");
 const btnPrev = document.querySelector("#prev");
+const galleryImages = document.querySelectorAll(".gallery-img");
+const totalImages = galleryImages.length;
 
 let slideIndex = 0;
 let timeOut;
-let initialPosition = 0;
-let maxPosition = 7;
 const slideWidth = document.querySelector(".image-container").offsetWidth;
 
 btnNext.addEventListener("click", () => {
   slideIndex -= 45;
   clearInterval(timeOut);
   updateGallery();
+
+  //  adding button pressed affect
+  btnNext.classList.add("btn-pressed");
+  setTimeout(() => {
+    btnNext.classList.remove("btn-pressed");
+  }, 300);
+
+  if (slideIndex === totalImages) {
+    btnNext.disabled = true;
+  }
 });
+
 btnPrev.addEventListener("click", () => {
   slideIndex += 45;
   clearInterval(timeOut);
   updateGallery();
+
+  //  adding button pressed affect
+  btnPrev.classList.add("btn-pressed");
+  setTimeout(() => {
+    btnPrev.classList.remove("btn-pressed");
+  }, 300);
+
+  if (slideIndex < 0) {
+    btnNext.disabled = false;
+  }
 });
 
 const updateGallery = function () {
   imageContainer.style.transform = `perspective(1000px) rotateY(${slideIndex}deg)`;
 
-  timeOut = setTimeout(() => {
-    slideIndex -= 45;
-    updateGallery();
-  }, 3000);
+  //   timeOut = setTimeout(() => {
+  //     slideIndex -= 45;
+  //     updateGallery();
+  //   }, 4000);
+
+  //   disabling buttons on first and last slides on the gallery
+  if (slideIndex === (totalImages - 1) * -45) {
+    btnNext.disabled = true;
+    btnNext.style.opacity = 0;
+    btnNext.style.cursor = "default";
+  } else {
+    btnNext.style.opacity = 0.5;
+    btnNext.style.cursor = "pointer";
+  }
+
+  if (slideIndex === 0) {
+    btnPrev.disabled = true;
+    btnPrev.style.opacity = 0;
+    btnPrev.style.cursor = "default";
+  } else {
+    btnPrev.disabled = false;
+    btnPrev.style.opacity = 0.5;
+    btnPrev.style.cursor = "pointer";
+  }
 };
 
 updateGallery();
